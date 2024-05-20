eaa65006e79a1c320cb66f936fc45f964ea06bc2
JackZhang1988 <jianbo1988@gmail.com>
2024-05-20 18:12:44 +0800

    feat: replace setting page with antd

--- src/renderer/src/pages/settings/index.tsx
+++ src/renderer/src/pages/settings/index.tsx
@@ -1,10 +1,8 @@
-import { Box } from '@mui/material'
-
 function SettingsPage(): JSX.Element {
   return (
-    <Box className="app-page page-settings">
+    <div className="app-page page-settings">
       <h2>settings</h2>
-    </Box>
+    </div>
   )
 }
 
