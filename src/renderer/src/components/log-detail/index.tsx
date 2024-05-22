43d3b286386fc6824ab652c099be5497375fa657
JackZhang1988 <jianbo1988@gmail.com>
2024-05-22 10:47:30 +0800

    refactor: update log-detail with antd

--- src/renderer/src/components/log-detail/index.tsx
+++ src/renderer/src/components/log-detail/index.tsx
@@ -1,78 +1,13 @@
 import './log-detail.less'
 
-import { SyntheticEvent, useState } from 'react'
+import { Collapse, CollapseProps, Descriptions, Tabs, TabsProps } from 'antd'
 
 import ReactJson from '@microlink/react-json-view'
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp'
-import { Box, Stack, Typography } from '@mui/material'
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion'
-import MuiAccordionDetails from '@mui/material/AccordionDetails'
-import MuiAccordionSummary, { AccordionSummaryProps } from '@mui/material/AccordionSummary'
-import { styled } from '@mui/material/styles'
-import Tab from '@mui/material/Tab'
-import Tabs from '@mui/material/Tabs'
 import { Log } from '@shared/contract'
 
 interface LogDetailProps {
   log: Log
 }
-
-const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
-    borderBottom: 0
-  },
-  '&::before': {
-    display: 'none'
-  }
-}))
-
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-    {...props}
-  />
-))(({ theme }) => ({
-  backgroundColor:
-    theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(0, 0, 0, .03)',
-  flexDirection: 'row-reverse',
-  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)'
-  },
-  '& .MuiAccordionSummary-content': {
-    marginLeft: theme.spacing(1)
-  }
-}))
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)'
-}))
-
-interface TabPanelProps {
-  children?: React.ReactNode
-  index: number
-  value: number
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      aria-labelledby={`log-tab-${index}`}
-      className="log-tab-panel"
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 1 }}>{children}</Box>}
-    </div>
-  )
-}
-
 interface LogObjBlockProps {
   data: Record<string, string>
 }
@@ -160,120 +95,121 @@ function isImg(type: string | undefined) {
 }
 
 function LogDetail({ log }: LogDetailProps): JSX.Element {
-  const [value, setValue] = useState(0)
-
-  const handleTabChange = (_: SyntheticEvent, newValue: number) => {
-    setValue(newValue)
-  }
-
   const requestParams = getRequestParams(log)
-  return (
-    <>
-      <Tabs aria-label="log detail tab" onChange={handleTabChange} value={value}>
-        <Tab label="Request"></Tab>
-        <Tab label="Response"></Tab>
-        <Tab label="Preview"></Tab>
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        <Accordion defaultExpanded={true}>
-          <AccordionSummary>
-            <Typography>General:</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack spacing={2}>
-              <Typography>Request URL: {log.url}</Typography>
-              <Typography>Request Method: {log.method}</Typography>
-              <Typography>Remote Address: {`${log.remoteIp}:${log.remotePort}`}</Typography>
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion defaultExpanded={true}>
-          <AccordionSummary>
-            <Typography>Request Header:</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <LogObjBlock data={log.requestHeaders} />
-          </AccordionDetails>
-        </Accordion>
-        <Accordion defaultExpanded={true}>
-          <AccordionSummary>
-            <Typography>Request Parameters:</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            {requestParams.isJson && (
-              <ReactJson
-                src={requestParams.data}
-                theme="monokai"
-                displayDataTypes={false}
-                name={false}
-              />
-            )}
-            {!requestParams.isJson && <div className="raw-content">{requestParams.data}</div>}
-          </AccordionDetails>
-        </Accordion>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Accordion defaultExpanded={true}>
-          <AccordionSummary>
-            <Typography>General:</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack spacing={2}>
-              <Typography>Status Code: {log.status}</Typography>
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion defaultExpanded={true}>
-          <AccordionSummary>
-            <Typography>Response Header:</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <LogObjBlock data={log.responseHeaders} />
-          </AccordionDetails>
-        </Accordion>
-        <Accordion defaultExpanded={true}>
-          <AccordionSummary>
-            <Typography>Response Body:</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <div className="raw-content">{log.responseBodyInfo?.bodyText}</div>
-          </AccordionDetails>
-        </Accordion>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        {log.responseBodyInfo?.isJson && (
-          <ReactJson
-            src={log.responseBodyInfo?.data}
-            theme="monokai"
-            displayDataTypes={false}
-            name={false}
-          />
-        )}
-        {isImg(log.responseBodyInfo?.type) && (
-          <>
-            <img src={log.url} alt="preview" />
-            {/* <object
-              data={log.responseBodyInfo?.data}
-              type={log.responseHeaders['content-type'] || 'image/png'}
-              width={400}
-              height={400}
-            >
-              Preview not available
-            </object> */}
-          </>
-        )}
-        {log.responseBodyInfo?.type === 'html' && (
-          <iframe
-            className="iframe-preview"
-            sandbox=""
-            frameBorder={0}
-            srcDoc={log.responseBodyInfo?.data}
-            title="preview"
-          />
-        )}
-      </TabPanel>
-    </>
-  )
+
+  const requestColItems: CollapseProps['items'] = [
+    {
+      key: 'general',
+      label: 'General',
+      children: (
+        <Descriptions
+          size="small"
+          column={1}
+          items={[
+            {
+              key: 'Request URL',
+              label: 'Request URL',
+              children: [log.url]
+            },
+            {
+              key: 'Request Method',
+              label: 'Request Method',
+              children: [log.method]
+            },
+            {
+              key: 'Remote Address',
+              label: 'Remote Address',
+              children: [`${log.remoteIp}:${log.remotePort}`]
+            }
+          ]}
+        />
+      )
+    },
+    {
+      key: 'requestHeader',
+      label: 'Request Header',
+      children: <LogObjBlock data={log.requestHeaders} />
+    },
+    {
+      key: 'requestParams',
+      label: 'Request Parameters',
+      children: requestParams.isJson ? (
+        <ReactJson src={requestParams.data} theme="monokai" displayDataTypes={false} name={false} />
+      ) : (
+        <div className="raw-content">{requestParams.data}</div>
+      )
+    }
+  ]
+
+  const responseColItems: CollapseProps['items'] = [
+    {
+      key: 'general',
+      label: 'General',
+      children: (
+        <Descriptions
+          column={1}
+          items={[
+            {
+              key: 'Status Code',
+              label: 'Status Code',
+              children: [log.status]
+            }
+          ]}
+        />
+      )
+    },
+    {
+      key: 'responseHeader',
+      label: 'Response Header',
+      children: <LogObjBlock data={log.responseHeaders} />
+    },
+    {
+      key: 'responseBody',
+      label: 'Response Body',
+      children: <div className="raw-content">{log.responseBodyInfo?.bodyText}</div>
+    }
+  ]
+
+  const items: TabsProps['items'] = [
+    {
+      key: 'request',
+      label: 'Request',
+      children: <Collapse size="small" items={requestColItems} />
+    },
+    {
+      key: 'response',
+      label: 'Response',
+      children: <Collapse size="small" items={responseColItems} />
+    },
+    {
+      key: 'preview',
+      label: 'Preview',
+      children: (
+        <>
+          {log.responseBodyInfo?.isJson && (
+            <ReactJson
+              src={log.responseBodyInfo?.data}
+              theme="monokai"
+              displayDataTypes={false}
+              name={false}
+            />
+          )}
+          {isImg(log.responseBodyInfo?.type) && <img src={log.url} alt="preview" />}
+          {log.responseBodyInfo?.type === 'html' && (
+            <iframe
+              className="iframe-preview"
+              sandbox=""
+              frameBorder={0}
+              srcDoc={log.responseBodyInfo?.data}
+              title="preview"
+            />
+          )}
+        </>
+      )
+    }
+  ]
+
+  return <Tabs defaultActiveKey="request" items={items} style={{ padding: '0 10px' }} />
 }
 
 export default LogDetail
